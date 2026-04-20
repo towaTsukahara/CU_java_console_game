749d4519dff71c96116f05f163c79fa2048fcf87
WIN-RUNI13INGAR\traininguser <towa.tsuskahara@skywill.jp>
2026-04-20 12:08:15 +0900

    feat:Item

--- Item.java
+++ Item.java
@@ -1,3 +1,15 @@
 public class Item {
-    
+    String name;
+    int healAmount;
+
+    public Item(String name, int healAmount){
+        this.name = name;
+        this.healAmount = healAmount;
+    }
+
+    public void showItem(){
+        System.out.println("=== Item Data ===");
+        System.out.println("name:" + name);
+        System.out.println("healAmount:" + healAmount);
+    }
 }
